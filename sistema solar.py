9a83802b0a93546307f9d3ffd44b59e3ffd80f39
Zhsn9 <117162237+MARcoo5@users.noreply.github.com>
2023-10-26 19:15:58 +0200

    sistema solar

---
+++ sistema solar.py
@@ -0,0 +1,9 @@
+import pygame
+
+ANCHO = 1080
+ALTO = 720
+CENTRO = (ANCHO/2, ALTO/2)
+NOMBRE = "sistema solar"
+
+VENTANA = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption(NOMBRE)
