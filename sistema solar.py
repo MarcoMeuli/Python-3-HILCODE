1b786bf8f1e26771d8ee2b6b047e3912e10128f1
Zhsn9 <117162237+MARcoo5@users.noreply.github.com>
2023-10-26 20:32:13 +0200

    sistema solar

--- sistema solar.py
+++ sistema solar.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 ANCHO = 1080
 ALTO = 720
@@ -7,3 +8,51 @@ NOMBRE = "sistema solar"
 
 VENTANA = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption(NOMBRE)
+
+
+def movimiento(radio, centro_x, centro_y, angulo):
+    x = centro_x + int(math.cos(angulo)*radio)
+    y = centro_y + int(math.sin(angulo)*radio)
+    return x, y
+
+
+x_tierra = CENTRO[0]+400
+y_tierra = CENTRO[1]
+angulo_tierra = 0
+velocidad_angular = 0.01
+
+
+
+
+ejecutar = True
+while ejecutar == True:
+    pygame.draw.circle(VENTANA, (255, 255, 0), CENTRO, 80)
+
+    pygame.draw.circle(VENTANA, (252, 34, 0), (CENTRO[0]+200,CENTRO[1]), 5)
+    pygame.draw.circle(VENTANA, (197, 149, 0), (CENTRO[0]+300,CENTRO[1]), 8)
+
+
+    x, y = movimiento(angulo_tierra, x_tierra, y_tierra, 10)
+
+    x +=velocidad_angular
+    y +=velocidad_angular
+
+    pygame.draw.circle(VENTANA, (28, 156, 0), (x,y), 15)
+
+
+
+
+
+
+
+
+
+
+    for eventos in pygame.event.get():
+        if eventos.type == pygame.QUIT:
+            ejecutar = False
+
+    pygame.display.update()
+
+
+pygame.quit()
