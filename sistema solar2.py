4867a8c40198d7c71f4563aafc57ff5ffefb2cfa
Zhsn9 <117162237+MARcoo5@users.noreply.github.com>
2023-11-16 19:14:57 +0100

    sistema solar2

---
+++ sistema solar2.py
@@ -0,0 +1,12 @@
+def constructorPlanetas(tamaño,radio,angulo,velocidad_angular,centro_x,centro_y,color):
+    objeto = {
+                "tamaño": tamaño,
+                "radio": radio,
+                "angulo": angulo,
+                "velocidad_angular": velocidad_angular,
+                "centro_x": centro_x,
+                "centro_y": centro_y,
+                "color": color,
+                "tipo": "planeta"
+            }
+    return objeto
