5778412565816387bcdede02ba1d8ee6e45055ce
Zhsn9 <117162237+MARcoo5@users.noreply.github.com>
2023-11-30 20:25:30 +0100

    sistema solar2

--- sistema solar2.py
+++ sistema solar2.py
@@ -14,21 +14,71 @@ def constructorPlanetas(nombre,tam, radio, color, x=0, y=0, angulo=0, v_angular=
             "color": color,
             "tipo": "planeta"
             }
-
-
     return objeto
 
 
 
 
-
-
-
+#########
+#########
 def setNombre(objeto, nombre):
-    return objeto["nombre"] = nombre
+    if "nombre" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["nombre"] = nombre
+        return  objeto
+    elif type(nombre) == str:
+        objeto["nombre"] = nombre
+        return  objeto
+    else:
+        return None
 
 def getNombre(objeto):
+    return objeto["nombre"]
+
+
+
+
+def setTam(objeto, tam):
+    if "tam" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["tam"] = tam
+    elif tam <= 0:
+        objeto["tam"] = tam
+    else:
+        print("Objeto invalido.")
     return objeto
+
+def getTam(objeto):
+    return objeto["tam"]
+
+
+
+
+def setRadio(objeto, radio):
+    if "radio" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["radio"] = radio
+    elif tam <= 0:
+        objeto["radio"] = radio
+    else:
+        print("Objeto invalido.")
+    return objeto
+
+def getRadio(objeto):
+    return objeto["radio"]
+
+
+
+
+def setAngulo(objeto, angulo):
+    if "angulo" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["angulo"] = angulo
+    elif angulo <= 0:
+        objeto["angulo"] = angulo
+    else:
+        print("Objeto invalido.")
+    return objeto
+
+def getAngulo(objeto):
+    return objeto["angulo"]
+
 
 
 
@@ -47,24 +97,51 @@ def setVelocidad(objeto=None, nombre=None):
             }
     if nombre != None and nombre in astros and objeto == None:
         velocidad = astros[nombre]
+        return velocidad
     elif nombre not in astros and nombre == None:
         velocidad = 0.3/random.randint(1,100)
+        return velocidad
     elif "tipo" in objeto and "nombre" in objeto and objeto["tipo"] == "planeta":
-        velocidad = astros[objeto["nombre"]]
+        objeto["v_angular"] = nueva_velocidad
+        return velocidad
     else:
         print("Objeto invalido.")
-    return velocidad
-
+    return objeto
 
 def getVelocidad(objeto):
-    if v_angular in objeto and objeto["tipo"] == "planeta":
-        return objeto[v_angular]
+    if "v_angular" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        return objeto["v_angular"]
     else:
-        return None
+        print("Objeto invalido.")
+    return objeto
 
 
 
 
+def setX(objeto, x):
+    if "x" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["x"] = x
+    else:
+        print("Objeto invalido.")
+    return objeto
+
+def getX(objeto):
+    return objeto["x"]
+
+
+
+
+def setY(objeto, y):
+    if "y" in objeto and "tipo" in objeto and objeto["tipo"] == "planeta":
+        objeto["y"] = y
+    else:
+        print("Objeto invalido.")
+    return objeto
+
+def getY(objeto):
+    return objeto["y"]
+#########
+#########
 
 
 
@@ -81,6 +158,7 @@ jupiter = constructorPlanetas(nombre="jupiter", tam=23, radio=250, color=(131,48
 saturno = constructorPlanetas(nombre="saturno", tam=18, radio=300, color=(252,200,0))
 urano = constructorPlanetas(nombre="urano", tam=15, radio=350, color=(107,217,255))
 neptuno = constructorPlanetas(nombre="neptuno", tam=8, radio=400, color=(0,100,255))
+_PC_ = {"RAM":8, "pantalla":"4K"}
 
+print(getTam(objeto=mercurio))
 
-print(saturno)
