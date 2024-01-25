0de46e61f3b4c673f7b7cd225e0d863ad3f36b17
Zhsn9 <117162237+MARcoo5@users.noreply.github.com>
2024-01-25 20:11:38 +0100

    TERMADO

--- sistema solar 2/sistema solar main.py
+++ sistema solar 2/sistema solar main.py
@@ -7,6 +7,10 @@ ANCHO = 1080
 ALTO = 720
 CENTRO = (ANCHO/2, ALTO/2)
 NOMBRE = "sistema solar"
+ejecutar = True
+N_asteroides = 600
+N_anillos = 100
+
 
 VENTANA = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption(NOMBRE)
@@ -18,25 +22,28 @@ def movimiento(radio, centro_x, centro_y, angulo):
 
 
 sol = planets_utils.constructorPlanetas(nombre="sol", tam=35, x=CENTRO[0], y=CENTRO[1], radio=0, color=(255,255,0))
-mercurio = planets_utils.constructorPlanetas(nombre="mercurio", tam=3, radio=40, color=(162,162,162))
+mercurio = planets_utils.constructorPlanetas(nombre="mercurio", tam=3, radio=45, color=(162,162,162))
 venus = planets_utils.constructorPlanetas(nombre="venus", tam=6, radio=60, color=(228,163,98))
 tierra = planets_utils.constructorPlanetas(nombre="tierra", tam=10, radio=90, color=(28,156,0))
 marte = planets_utils.constructorPlanetas(nombre="marte", tam=12, radio=125, color=(252,34,0))
-jupiter = planets_utils.constructorPlanetas(nombre="jupiter", tam=23, radio=280, color=(131,48,21))
-saturno = planets_utils.constructorPlanetas(nombre="saturno", tam=18, radio=330, color=(252,200,0))
+jupiter = planets_utils.constructorPlanetas(nombre="jupiter", tam=23, radio=260, color=(131,48,21))
+saturno = planets_utils.constructorPlanetas(nombre="saturno", tam=18, radio=320, color=(204,102,0))
 urano = planets_utils.constructorPlanetas(nombre="urano", tam=15, radio=370, color=(107,217,255))
 neptuno = planets_utils.constructorPlanetas(nombre="neptuno", tam=8, radio=400, color=(0,100,255))
 luna = planets_utils.constructorPlanetas(nombre="luna", tam=2, radio=20, color=(200,200,200))
 planetas = [sol, mercurio, venus, tierra, marte, jupiter, saturno, urano, neptuno, luna]
 
-for i in range(10):
-    planetas.append(planets_utils.constructorPlanetas(nombre="asteroides", tam=3, radio=random.randint(135, 270), color=(255,255,255)))
+for i in range(N_asteroides):
+    planetas.append(planets_utils.constructorPlanetas(nombre="asteroides", tam=random.randint(1,3), radio=random.randint(150, 220), color=(255,255,255)))
+for i in range(N_anillos):
+    planetas.append(planets_utils.constructorPlanetas(nombre="anillos", tam=random.randint(10,16)/10, radio=random.randint(23,28), color=(255,255,255)))
 
 for i in range(len(planetas)):
     planetas[i] = planets_utils.setX(planetas[i], CENTRO[0] + planets_utils.getRadio(planetas[i]))
     planetas[i] = planets_utils.setY(planetas[i], CENTRO[1])
+    if planets_utils.getNombre(planetas[i])== "asteroides" or planets_utils.getNombre(planetas[i])== "anillos":
+        planetas[i] = planets_utils.setAngulo(planetas[i], random.randint(0,1000))
 
-ejecutar = True
 while ejecutar == True:
     VENTANA.fill((0,0,0))
 
@@ -55,15 +62,18 @@ while ejecutar == True:
             centro_y = tierra["y"],
             angulo = planets_utils.getAngulo(planetas[i]))
 
+        elif planets_utils.getNombre(planetas[i]) == "anillos":
+            x, y = movimiento(radio = planets_utils.getRadio(planetas[i]),
+            centro_x = saturno["x"],
+            centro_y = saturno["y"],
+            angulo = planets_utils.getAngulo(planetas[i]))
+
         else:
             x, y = movimiento(radio = planets_utils.getRadio(planetas[i]),
             centro_x = CENTRO[0],
             centro_y = CENTRO[1],
             angulo = planets_utils.getAngulo(planetas[i]))
 
-
-        asteroides = planets_utils.setX(asteroides, x)
-        asteroides = planets_utils.setY(asteroides, y)
 
         planetas[i] = planets_utils.setX(planetas[i], x)
         planetas[i] = planets_utils.setY(planetas[i], y)
@@ -74,6 +84,20 @@ while ejecutar == True:
 
 
     for eventos in pygame.event.get():
+        if eventos.type == pygame.MOUSEBUTTONDOWN and eventos.button == 4:
+            for i in range(len(planetas)):
+                planetas[i]["tam"]+=planetas[i]["tam"]/5
+                planetas[i]["radio"]+=planetas[i]["radio"]/5
+
+
+
+        if eventos.type == pygame.MOUSEBUTTONDOWN and eventos.button == 5:
+            for i in range(len(planetas)):
+                planetas[i]["tam"]-=planetas[i]["tam"]/5
+                planetas[i]["radio"]-=planetas[i]["radio"]/5
+
+
+
         if eventos.type == pygame.QUIT:
             ejecutar = False
 
